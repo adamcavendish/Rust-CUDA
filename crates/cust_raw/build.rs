f5917f19d4c9b65b37f79cd94e6d35c45413b1d1
Adam Basfop Cavendish <GetbetterABC@yeah.net>
2025-03-24 20:33:50 +0800

    draft: chore(cust_raw): use live bindgen to generate CUDA headers

--- crates/cust_raw/build.rs
+++ crates/cust_raw/build.rs
@@ -1,3 +1,37 @@
+use std::env;
+use std::path;
+
 fn main() {
-    find_cuda_helper::include_cuda();
+    let cuda_root = find_cuda_helper::find_cuda_root()
+        .expect("CUDA must be installed to generate cust_raw bindings.");
+    let cuda_include_path = cuda_root.join("include");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .clang_arg(format!("-I{}", cuda_include_path.display()))
+        .allowlist_type("^CU.*")
+        .allowlist_type("^cuuint(32|64)_t")
+        .allowlist_type("^cudaError_enum")
+        .allowlist_type("^cu.*Complex$")
+        .allowlist_type("^cuda.*")
+        .allowlist_type("^libraryPropertyType.*")
+        .allowlist_var("^CU.*")
+        .allowlist_function("^cu.*")
+        .default_enum_style(bindgen::EnumVariation::Rust {
+            non_exhaustive: true,
+        })
+        .derive_default(true)
+        .derive_eq(true)
+        .derive_hash(true)
+        .derive_ord(true)
+        .size_t_is_usize(true)
+        .layout_tests(true)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = path::PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- crates/cust_raw/src/lib.rs
+++ crates/cust_raw/src/lib.rs
@@ -1,4 +1,5 @@
-#![allow(warnings)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-mod cuda;
-pub use cuda::*;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
